9c2ad622d9f806bc433323183a15b350ac33fa17
Prashanth Pai <prashanth.pai@plivo.com>
2021-03-16 17:01:43 +0530

    Add multi-threaded dequeuer
    Signed-off-by: Prashanth Pai <prashanth.pai@plivo.com>

--- dequeue_script.py
+++ dequeue_script.py
@@ -1,17 +1,64 @@
 import random
-from uuid import uuid4
-from time import sleep
+import signal
+import threading
+from collections import defaultdict, OrderedDict
 from sharq import SharQ
-sq = SharQ('./sharq.conf')
 
-while True:
-    response = sq.dequeue(queue_type='sms')
-    status = response.get('status', 'failure')
-    if status == 'success':
-        print(response)
-        if bool(random.getrandbits(1)):
-            response = sq.finish(queue_type='sms',
-                    job_id=response['job_id'],
-                    queue_id=response['queue_id'],
-	    )
-        print(response)
+
+STOP_THREADS = False
+THREAD_COUNT = 4
+
+deqs = defaultdict(int)
+acks = defaultdict(int)
+no_acks = defaultdict(int)
+
+
+def dequeue_worker(worker_id):
+    global STOP_THREADS, deqs, acks, no_acks
+
+    sq = SharQ('./sharq.conf')
+
+    while STOP_THREADS is False:
+        response = sq.dequeue(queue_type='sms')
+        if len(response) <= 1:
+            continue
+        status = response.get('status', 'failure')
+        if status == 'success':
+            deqs[response['job_id']] += 1
+            if bool(random.getrandbits(1)):
+                sq.finish(queue_type='sms',
+                          job_id=response['job_id'],
+                          queue_id=response['queue_id'])
+                acks[response['job_id']] += 1
+            else:
+                no_acks[response['job_id']] += 1
+
+    print("thread-{}: exiting".format(worker_id))
+
+
+if __name__ == '__main__':
+    threads = [threading.Thread(target=dequeue_worker,
+                                args=(x,),
+                                name=str(x)) for x in range(THREAD_COUNT)]
+
+    for t in threads:
+        t.start()
+
+    def signal_handler(sig, frame):
+        print("got signal: {}\n".format(signal.strsignal(sig)))
+        global STOP_THREADS
+        STOP_THREADS = True
+
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+
+    for t in threads:
+        t.join()
+    print("all threads ended")
+
+    print("\nDequeued:\n",
+          OrderedDict(sorted(deqs.items(), key=lambda x: int(x[0]))))
+    print("\nAcked:\n",
+          OrderedDict(sorted(acks.items(), key=lambda x: int(x[0]))))
+    print("\nNot Acked:\n",
+          OrderedDict(sorted(no_acks.items(), key=lambda x: int(x[0]))))
--- enqueue_script.py
+++ enqueue_script.py
@@ -1,17 +1,10 @@
-from uuid import uuid4
-from time import sleep
 from sharq import SharQ
+
 sq = SharQ('./sharq.conf')
 
-count = 0
-while True:
-    resp = sq.enqueue(job_id=str(uuid4()),
-            payload={'a': 'b'},
-            interval=10,  # in milliseconds.
-            queue_id='user001',
-            queue_type='sms'
-    )
-    sleep(0.1)
-    count = count + 1
-    if count > 100:
-        break
+for i in range(100):
+    resp = sq.enqueue(job_id=str(i),
+                      payload={'a': 'b'},
+                      interval=10,  # in milliseconds.
+                      queue_id='user001',
+                      queue_type='sms')
