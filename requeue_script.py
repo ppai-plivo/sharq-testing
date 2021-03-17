ae024ecb6e194a33a41fb32dde8ef496d17b0893
Prashanth Pai <prashanth.pai@plivo.com>
2021-03-17 11:24:23 +0530

    Make requeue multi-threaded
    Signed-off-by: Prashanth Pai <prashanth.pai@plivo.com>

--- requeue_script.py
+++ requeue_script.py
@@ -1,11 +1,40 @@
-from uuid import uuid4
+import signal
+import threading
 from time import sleep
 from sharq import SharQ
 
-sq = SharQ('./sharq.conf')
 
-while True:
-    response = sq.requeue()
-    if response is not None:
-        print(response)
-    sleep(0.1)
+THREAD_COUNT = 4
+REQUEUE_INTERVAL = 5
+STOP_THREADS = False
+
+
+def requeue_worker(worker_id):
+    sq = SharQ('./sharq.conf')
+    while STOP_THREADS is False:
+        response = sq.requeue()
+        if response is not None:
+            print(response)
+        sleep(REQUEUE_INTERVAL)
+    print("thread-{}: exiting".format(worker_id))
+
+
+if __name__ == '__main__':
+    threads = [threading.Thread(target=requeue_worker,
+                                args=(x,),
+                                name=str(x)) for x in range(THREAD_COUNT)]
+
+    for t in threads:
+        t.start()
+
+    def signal_handler(sig, frame):
+        print("got signal: {}\n".format(signal.strsignal(sig)))
+        global STOP_THREADS
+        STOP_THREADS = True
+
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+
+    for t in threads:
+        t.join()
+    print("all threads ended")
